90aad7e49bdd0014ff0c3ee980116e40818b4cf6
bekbekbek11f-cyber <bekbekbek11f@gmail.com>
2026-09-06 13:48:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ Bot sozlamalari.
 """
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8839353458:AAHrfZKJTfzAysXcs14d7LLxbPMIt2ajBZI")
 ADMIN_IDS = [8475619369]
 
 DB_PATH = "bot.db"

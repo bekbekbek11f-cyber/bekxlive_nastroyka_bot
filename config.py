256f69b2576dbca18fd2bcc024de2786f23eb939
bekbekbek11f-cyber <bekbekbek11f@gmail.com>
2026-09-06 13:17:20 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 """
 Bot sozlamalari.
 
-BOT_TOKEN     -> @BotFather dan olingan token
-ADMIN_IDS     -> admin panelidan foydalana oladigan Telegram user_id lar ro'yxati
-                 (o'z Telegram ID'ingizni bilish uchun @userinfobot ga /start yozing)
+BOT_TOKEN     -> 8839353458:AAHrfZKJTfzAysXcs14d7LLxbPMIt2ajBZI
+ADMIN_IDS     -> 8475619369
+        
 """
 
 BOT_TOKEN = "PUT_YOUR_BOT_TOKEN_HERE"
 
 ADMIN_IDS = [
-    123456789,  # <-- bu yerga o'z Telegram ID'ingizni yozing
+    8475619369,  # <-- bu yerga o'z Telegram ID'ingizni yozing
 ]
 
 DB_PATH = "bot.db"

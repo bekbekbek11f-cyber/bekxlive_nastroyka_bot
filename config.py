9274f849e7efe6221848ab45b8ef062e56702bd7
bekbekbek11f-cyber <bekbekbek11f@gmail.com>
2026-09-06 13:56:57 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 """
 Bot sozlamalari.
 """
-import os
 
-BOT_TOKEN = os.getenv("8839353458:AAHrfZKJTfzAysXcs14d7LLxbPMIt2ajBZI")
+# Bu yerga BotFather bergan haqiqiy tokeningizni qo'shtirnoq ichiga yozing
+BOT_TOKEN = "8839353458:AAHrfZKJTfzAysXcs14d7LLxbPMIt2ajBZI"
+
+# Bu yerga kvadrat qavs ichida o'z Telegram ID raqamingizni yozing
 ADMIN_IDS = [8475619369]
 
 DB_PATH = "bot.db"
-
-
